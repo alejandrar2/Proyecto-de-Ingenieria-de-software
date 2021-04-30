641589903ad15558748d501e318a4cff6f7305fa
Eddjc <eduardojose.campos@gmail.com>
2021-04-30 09:53:32 -0600

    html-css graficas

--- frontend-proyecto-ingenieria/src/app/paginas/graficas/graficas.component.ts
+++ frontend-proyecto-ingenieria/src/app/paginas/graficas/graficas.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
-import * as pluginDataLabels from 'chartjs-plugin-annotation';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import * as pluginDataLabels from 'chartjs-plugin-datalabels';
 import { Label } from 'ng2-charts';
-import { CategoriaService } from '../../servicios/categoria.service';
 
 @Component({
   selector: 'app-graficas',
@@ -22,59 +21,38 @@ export class GraficasComponent implements OnInit {
       }
     }
   };
-  public barChartLabels: Label[] = ['2010', '2011', '2012', '2013', '2014', '2015', '2016'];
-  //Tipo de grafico que queremos: ejem: line, bar, radar
+
+  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
   public barChartPlugins = [pluginDataLabels];
-  
-  //Datos que vamos a cargar en las graficas 
-  public barChartData: ChartDataSets[];
-  public chartColors:any;
 
-  private categoria:any;
-  private dato:any ;
-  //Arreglo de los datos que vamos a pasar
-  private datos = [];
-  //Arreglo de las categorias que vamos a pasar
-  private nombreCategoria = [];
-  //Arreglo de los colores que vamos a pasar
-  private colores = [];
+  public barChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+  ]
 
-  constructor(protected categoriaService: CategoriaService) {
-    this.getCategoria();
+  constructor() {}
+  ngOnInit() {}
+
+  // events
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
   }
 
-  ngOnInit() {
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
   }
 
-  getCategoria() {
-    this.categoriaService.obtenerCategorias().subscribe(res => {
-      this.categoria = res;
-      //Obtenemos las categorias y recorremos almacenando 
-      //en cada arreglo lo necesario
-      for (const cate of this.categoria) {
-        this.dato = cate.DATOS_CATE.split(',');
-         this.datos.push(this.dato);
-         this.nombreCategoria.push(cate.NOMBRE_CATE);
-         this.colores.push(cate.COLOR_CATE);
-      }
-     
-      //Llamado a función
-      this.cargarDatos(this.datos, this.nombreCategoria, this.colores);
-    });
+  public randomize(): void {
+    // Only Change 3 values
+    this.barChartData[0].data = [
+      Math.round(Math.random() * 100),
+      59,
+      80,
+      (Math.random() * 100),
+      56,
+      (Math.random() * 100),
+      40 ];
   }
-
-  //Función que carga los datos en la grafica, asi como los colores
-  cargarDatos(datos:any, nombreCategoria:any, colores:any) {
-    this.barChartData = [];
-    this.chartColors = [];
-    
-    for (const index in datos) {
-      this.barChartData.push({ data: datos[index], label: nombreCategoria[index] });
-      this.chartColors.push({backgroundColor: colores[index]});
-    }
-
-  }
-
 }
